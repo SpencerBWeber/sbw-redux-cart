07a8421bd57046cc15de1209c62d41f752f84f09
JingleDjango95 <spencerweber019@gmail.com>
2019-10-16 12:07:20 -0600

    now cart renders products

--- src/cart.js
+++ src/cart.js
@@ -1,13 +1,30 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import Item from "./item";
 
 const Cart = () => {
   const total = useSelector(state => state.products.total);
+  const cart = useSelector(state => state.products.cart);
+
+  const renderProducts = () => {
+    return cart.map(product => {
+      return (
+        <Item
+          key={product.id}
+          id={product.id}
+          name={product.name}
+          price={product.price}
+          description={product.description}
+        />
+      );
+    });
+  };
 
   return (
     <div className="">
       <h1>Cart</h1>
       <h3>Total: ${total.toFixed(2)}</h3>
+      <div className="products">{renderProducts()}</div>
     </div>
   );
 };
