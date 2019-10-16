7ae2b187404ea641224a00a2ff2709391ced3ec5
JingleDjango95 <spencerweber019@gmail.com>
2019-10-16 11:10:12 -0600

    put everything from app.js into index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { useRoutes, A } from "hookrouter";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Home from "./home";
+import Cart from "./cart";
+import "./App.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const routes = {
+  "/": () => <Home />,
+  "/cart": () => <Cart />
+};
+
+function App() {
+  return (
+    <div className="App">
+      <A href="/">Home</A>
+      <A href="/cart">Cart</A>
+      {useRoutes(routes)}
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
